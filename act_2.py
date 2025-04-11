89c051de614f51a297562845f058a7502e6e11fa
NetAcad DEVASC <changeme@example.com>
2025-04-11 18:07:11 +0000

    commit act 1 y 2

---
+++ act_2.py
@@ -0,0 +1,11 @@
+#act 2
+
+nombre = input("ingrese su nombre ")
+apellido = input("ingrese su apellido ")
+seccion = input("ingrese su seccion ")
+sede = input("ingrese su sede ")
+
+print ("--infromacion ingresada--")
+print (f"Nombre Completo: {nombre} {apellido}")
+print (f"Seccion: {seccion}")
+print (f"Sede: {sede}")
---
+++ acti_1.py
@@ -0,0 +1,8 @@
+#scrit 1
+print ("Ejercicio 1 Programación y Redes Virtualizadas")
+ 
+integrantes = ["joaquin", "pepe", "kairo"]
+
+print ("integrantes del grupo")
+for nombre in integrantes:
+    print("-",nombre)
